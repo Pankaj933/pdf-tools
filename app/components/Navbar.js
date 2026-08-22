5c95d296cb0c516d1fcdb21975b505c3744ff8d4
Pankaj933 <pankajmaurya24042003@gmail.com>
2026-08-22 19:33:49 +0530

    chnage

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -151,7 +151,7 @@ export default function Navbar() {
 
             {/* Tools */}
             <Link
-              href="/#tools"
+              href="/tools"
               onClick={() => setMobileMenuOpen(false)}
               className="block font-medium text-slate-700 hover:text-blue-600 transition-colors"
             >
